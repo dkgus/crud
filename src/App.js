8160f5868d9190975f3b83465cf895657b8508b2
dkgus12dkgus <dkgus12dkgus@naver.com>
2023-03-23 13:51:14 +0900

    FIX:수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 // import Crud from "./components/Crud";
 // import DynamicColumn from "./components/DynamicColumn";
-import DynamicChildColumn from "./components/DynamicChildColumn";
+//import DynamicChildColumn from "./components/DynamicChildColumn";
 import SelectType from "./components/SelectType";
 function App() {
   return (
@@ -9,8 +9,8 @@ function App() {
       {/* <Crud />
       DynamicColumn
       <DynamicColumn /> */}
-      DynamicChildColumn
-      <DynamicChildColumn />
+      {/* DynamicChildColumn
+      <DynamicChildColumn /> */}
       SelectType
       <SelectType />
     </div>
