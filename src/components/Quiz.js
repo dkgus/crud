80edbc55bb8a42f8b3918ad945a874fa37ff3116
dkgus12dkgus <dkgus12dkgus@naver.com>
2023-03-24 13:59:24 +0900

    ADD: quiz생성

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,49 +1,72 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Radio } from "antd";
 
 function Quiz() {
-  const [start, setStart] = useState(false);
   const questions = [
     {
       order: 1,
       question: "What is 2*5?",
       choices: [2, 5, 10, 15, 20],
-      correctAnswer: 2,
+      correctAnswer: 10,
     },
     {
       order: 2,
       question: "What is 3*6?",
-      choices: [3, 6, 9, 12, 18],
-      correctAnswer: 4,
+      choices: [3, 4, 9, 12, 18],
+      correctAnswer: 18,
     },
     {
       order: 3,
       question: "What is 8*9?",
-      choices: [72, 99, 108, 134, 156],
-      correctAnswer: 0,
+      choices: [4, 72, 108, 134, 156],
+      correctAnswer: 72,
     },
     {
       order: 4,
       question: "What is 1*7?",
-      choices: [4, 5, 6, 7, 8],
-      correctAnswer: 3,
+      choices: [4, 3, 6, 7, 8],
+      correctAnswer: 7,
     },
     {
       order: 5,
       question: "What is 8*8?",
-      choices: [20, 30, 40, 50, 64],
-      correctAnswer: 4,
+      choices: [20, 4, 40, 50, 64],
+      correctAnswer: 64,
     },
   ];
+  const [start, setStart] = useState(false);
+  const [answer, setAnswer] = useState("");
+  const [count, setCount] = useState(1);
+  const [answerSheet, setAnswerSheet] = useState([]);
+
+  useEffect(() => {}, [answer, count]);
 
   const onClick = () => {
     setStart(true);
   };
+
+  const onChange = (e, rightAs) => {
+    let answerArr = [...answerSheet];
+    //답이 맞을 때에만 배열에 들어가게끔
+    if (e.target.value === rightAs) {
+      setAnswer(e.target.value);
+      answerArr.push(e.target.value);
+    }
+    setAnswerSheet(answerArr);
+  };
+
+  const nextBtn = () => {
+    setCount(count + 1);
+  };
+  const prevBtn = () => {
+    setCount(count - 1);
+  };
+
   return (
     <div>
       Dynamic Quiz!
       <div>do you wanna start?</div>
-      <Button onClick={onClick}>
+      <Button onClick={onClick} disabled={start ? true : false}>
         {start ? "you are playing already!" : "Start!"}
       </Button>
       <div>
@@ -51,26 +74,44 @@ function Quiz() {
           <>
             {questions &&
               questions.map((item) => {
-                console.log("item.choices", item.choices);
-
-                if (item.order === 1) {
+                if (item.order === count) {
                   return (
                     <>
-                      <div>{item.question}</div>
+                      <div style={{ paddingTop: 30 }}>{item.question}</div>
                       <div>
-                        {item.choices.map((elem, idx) => (
-                          <Radio.Group>
-                            <Radio value={elem}>{elem}</Radio>
-                          </Radio.Group>
-                        ))}
+                        <Radio.Group>
+                          {item.choices.map((elem) => (
+                            <Radio
+                              value={elem}
+                              onChange={(e) => onChange(e, item.correctAnswer)}
+                            >
+                              {elem}
+                            </Radio>
+                          ))}
+                        </Radio.Group>
                       </div>
                     </>
                   );
+                } else if (questions.length < count || questions.length === 0) {
+                  return null;
                 }
               })}
             <div>
-              {/*
-              <Radio.Group options={plainOptions} onChange={onChange1} value={value1} /> */}
+              {questions.length < count ? (
+                <>당신의 정답 개수는 {answerSheet.length}개 입니다 !</>
+              ) : null}
+
+              <div style={{ paddingTop: 30 }}>
+                <Button onClick={prevBtn} disabled={count < 1 ? true : false}>
+                  prev
+                </Button>
+                <Button
+                  onClick={nextBtn}
+                  disabled={questions.length < count ? true : false}
+                >
+                  Next
+                </Button>
+              </div>
             </div>
           </>
         ) : null}
